a7391ebe64db9037b89765ecd23768f28cd0d904
michele-pupo <mikypupo@hotmail.it>
2024-04-09 17:36:19 +0200

    second page link

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -7,5 +7,12 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index(){
+        return view('home');
+    }
+
+    public function showMovies(){
+        return view('movie');
+    }
+
 }
